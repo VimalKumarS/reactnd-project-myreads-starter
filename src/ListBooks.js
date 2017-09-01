a84926f1c60825bb1d02e39d3d19b5ae34ae777f
VIMALKUMARPC\vimal <vimalkumarsinghal@gmail.com>
2017-09-01 00:35:51 -0700

    React book search library repo

---
+++ src/ListBooks.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+class ListBooks extends React.Component {
+  static propTypes = {
+    books: PropTypes.object.isRequired
+  }
+
+  componentDidMount() {
+    //console.log(this.props.books)
+  }
+
+  render() {
+    const {books, onSelfChange} = this.props
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {Object
+              .keys(books)
+              .map((key) => (
+                <div className="bookshelf" key={books[key].Title}>
+                  <h2 className="bookshelf-title">
+                    {books[key].Title}</h2>
+                  <div className="bookshelf-books"></div>
+
+                  <ol className="books-grid">
+                    {books[key]
+                      .books
+                      .map((detail, i) => (
+
+                        <li key={detail.id}>
+                          <div className="book">
+                            <div className="book-top">
+                              
+                              <div
+                                className="book-cover"
+                                style={{
+                                width: 128,
+                                height: 193,
+                                backgroundImage: `url(${detail.imageLinks.thumbnail})`
+                              }}></div>
+                              <div className="book-shelf-changer">
+                                <select onChange={(e) => onSelfChange(detail, e.target.value)} value={detail.shelf}>
+                                  <option value="none" disabled>Move to...</option>
+
+                                  <option value="currentlyReading" >Currently Reading</option>
+
+                                  <option value="wantToRead" >Want to Read</option>
+
+                                  <option value="read" >Read</option>
+
+                                  <option value="none">None</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div className="book-title">{detail.title}</div>
+                            <div className="book-authors">{detail
+                                .authors
+                                .join()}</div>
+                          </div>
+                        </li>
+                      ))}
+                  </ol>
+
+                </div>
+              ))}
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ListBooks;
---
+++ src/SearchBooks.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import { Link } from 'react-router-dom'
+class SearchBooks extends React.Component {
+state = { 
+  books: []
+}
+componentDidMount() {
+   this.setState({books: []})
+}
+GetImageUrl(detail){
+return detail.imageLinks !== undefined ? detail.imageLinks.thumbnail : ''
+  }
+searchBook(query){
+  if(query.trim() !=''){
+  BooksAPI.search(query.trim(),20).then(res => {    
+    this.setState({books: res})
+  })}
+  else{
+    this.setState({books: []})
+  }
+}
+onSelfChange(detail,shelf){
+  console.log(detail);
+  let _books = this.state.books.filter(key => key.id !== detail.id);
+  this.setState({books:_books})
+  this.props.onSelfChange(detail,shelf);
+}
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">
+         Close</Link>
+          <div className="search-books-input-wrapper">
+            {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+            <input type="text" placeholder="Search by title or author"  onChange={(event) => this.searchBook(event.target.value)}/>
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+             {this.state.books
+                      .map((detail, i) => (
+
+                       <li key={detail.id}>
+                          <div className="book">
+                            <div className="book-top">
+                               
+                              <div
+                                className="book-cover"
+                                style={{
+                                width: 128,
+                                height: 193,
+                                backgroundImage: `url(${this.GetImageUrl(detail)})`
+                              }}></div>
+                              <div className="book-shelf-changer">
+                                <select onChange={(e) => this.onSelfChange(detail, e.target.value)} value="none">
+                                  <option value="disable" disabled>Move to...</option>
+
+                                  <option value="currentlyReading" >Currently Reading</option>
+
+                                  <option value="wantToRead" >Want to Read</option>
+
+                                  <option value="read" >Read</option>
+
+                                  <option value="none">None</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div className="book-title">{detail.title}</div>
+                            <div className="book-authors">{detail.authors !== undefined ? detail.authors
+                                .join() : detail.authors}</div>
+                          </div>
+                        </li>
+                      ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SearchBooks;
